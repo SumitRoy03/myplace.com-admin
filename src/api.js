d1a4b074a99e217cfb70b4646c31a14a70d53be8
SumitRoy03 <rsumit647@gmail.com>
2023-01-01 18:34:11 +0530

    icon added

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: `${process.env.REACT_APP_SERVER_URL}/api`,
   withCredentials: true
 });
